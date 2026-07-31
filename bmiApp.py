5413f15ba3742631d437f5dd11e809cbf48c06cf
406-25-nattapat <54550@yupparaj.ac.th>
2026-07-31 10:59:19 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -12,7 +12,7 @@ if st.button("คำนวณค่า BMI"):
    bmi = weight / (height_m ** 2)
 
 st.write("___")
-st.header(f"ค่า BMI ของคุณคือ: **{bmi: .2f}**")
+st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
 if bmi < 18.5:
    st.warning("คุณมีน้ำหนักน้อยกว่าเกณฑ์")
